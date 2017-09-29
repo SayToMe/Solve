5af809239ca882df86c5ee2ca4d6c72d7ca540ff
Qrtic <smirnov_pasha@mail.ru>
2017-09-29 03:13:19 +0300

    first pass migrating to scaffold structure

---
+++ tests/Solve.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module Solve.Tests
+
+open Solve
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
