5345723e75e25ebb0e5d9231b1e0796c98a02ecc
Qrtic <smirnov_pasha@mail.ru>
2017-09-29 03:38:58 +0300

    Added more tests + improved structure

--- tests/Solve.Tests/Tests.fs
+++ tests/Solve.Tests/Tests.fs
@@ -16,108 +16,139 @@ let sv x = AnyVariable(Variable(x))
 let snp x = Parameter(sn x)
 [<DebuggerStepThrough>]
 let vp n = Parameter(AnyVariable(Variable(n)))
+[<DebuggerStepThrough>]
+let charP c = Parameter(AnyTyped(TypedSChar(SChar(c))))
 
 [<DebuggerStepThrough>]
 let sna x = Argument(sn x)
 [<DebuggerStepThrough>]
 let va n = Argument(AnyVariable(Variable(n)))
+[<DebuggerStepThrough>]
+let charA c = Argument(AnyTyped(TypedSChar(SChar(c))))
 
-[<Test>]
-let testUnifyParamsWithArguments() =
-    let u1 = ExecutionModule.unifyParamsWithArguments [vp "N"] [va "N2"] |> check "u1" (Some([sv "N"]))
-    let u2 = ExecutionModule.unifyParamsWithArguments [snp 1.] [va "N"] |> check "u2" (Some([sn 1.]))
-    let u3 = ExecutionModule.unifyParamsWithArguments [vp "N"] [sna 1.] |> check "u3" (Some([sn 1.]))
-    let u4 = ExecutionModule.unifyParamsWithArguments [snp 1.] [sna 1.] |> check "u4" (Some([sn 1.]))
-    let u5 = ExecutionModule.unifyParamsWithArguments [snp 1.] [sna 2.] |> check "u5" None
-    ()
+[<DebuggerStepThrough>]
+let charAny c = AnyTyped(TypedSChar(SChar(c)))
 
-[<Test>]
-let testUnifyExpression() = 
-    let expr1 = 
+[<DebuggerStepThrough>]
+let stringAny (str: string) = AnyTyped(TypedSList(SList(str.ToCharArray() |> Array.map (SChar >> TypedSChar) |> Array.toList)))
+
+[<TestFixture>]
+module SimpleTests =
+    [<Test>]
+    let testUnifyParamsWithArguments() =
+        ExecutionModule.unifyParamsWithArguments [vp "N"] [va "N2"] |> check "u1" (Some([sv "N"]))
+        ExecutionModule.unifyParamsWithArguments [snp 1.] [va "N"] |> check "u2" (Some([sn 1.]))
+        ExecutionModule.unifyParamsWithArguments [vp "N"] [sna 1.] |> check "u3" (Some([sn 1.]))
+        ExecutionModule.unifyParamsWithArguments [snp 1.] [sna 1.] |> check "u4" (Some([sn 1.]))
+        ExecutionModule.unifyParamsWithArguments [snp 1.] [sna 2.] |> check "u5" None
+
+    [<Test>]
+    let testUnifyExpression() = 
         ExecutionModule.unifyExpression (EqExpr(sv "N", sn 1.)) (fun (Variable(v)) -> sn 1.)
         |> check "unif1" (EqExpr(sn 1., sn 1.))
-    let expr2 = 
         ExecutionModule.unifyExpression (EqExpr(sv "N", sv "N2")) (fun (Variable(v)) -> if v = "N" then sn 1. else sn 2.)
         |> check "unif2" (EqExpr(sn 1., sn 2.))
-    let expr3 = 
         ExecutionModule.unifyExpression (EqExpr(sv "N", sv "N2")) (fun (Variable(v)) -> if v = "N" then sn 1. else sv v)
         |> check "unif3" (EqExpr(sn 1., sv "N2"))
-    ()
     
-[<Test>]
-let testUnifyRule() = 
-    let rule1 = 
+    [<Test>]
+    let testUnifyRule() = 
         ExecutionModule.unifyRule (Rule(Signature("eq1", [vp "N"]), (EqExpr(sv "N", sn 1.)))) [sna 1.]
         |> check "rule1" (Some(Rule(Signature("eq1", [snp 1.]), (EqExpr(sn 1., sn 1.)))))
-    ()
     
-[<Test>]
-let testExecuteCalc() = 
-    let calc1 = 
+    [<Test>]
+    let testExecuteCalc() = 
         ExecutionModule.executeCalc (Value(CalcAny(sn 1.)))
         |> check "calc1" (SNumber(1.))
-    let calc2 = 
+
         ExecutionModule.executeCalc (Plus(CalcAny(sn 1.), CalcAny(sn 1.)))
         |> check "calc2" (SNumber(2.))
-    ()
     
-[<Test>]
-let testExecuteExpression() = 
-    let executeCustom a = failwith "unexpected input"
-    let exExp1 = 
+    [<Test>]
+    let testExecuteExpression() = 
+        let executeCustom a = failwith "unexpected input"
+    
         ExecutionModule.executeExpression (EqExpr(sv "N", sn 1.)) executeCustom (fun v -> sn 1.)
         |> check "ex exp1" [EqExpr(sn 1., sn 1.)]
-    let exExp2 = 
         ExecutionModule.executeExpression (EqExpr(sv "N", sn 1.)) executeCustom (fun v -> AnyVariable(v))
         |> check "ex exp1" [EqExpr(sn 1., sn 1.)]
-    let exExp3 = 
         ExecutionModule.executeExpression (AndExpression(CalcExpr(sv "N", Value(CalcAny(sn 1.))), EqExpr(sv "N", sn 1.))) executeCustom (fun v -> sn 1.)
         |> check "ex exp3" [AndExpression(CalcExpr(sn 1., Value(CalcAny(sn 1.))), EqExpr(sn 1., sn 1.))]
-    ()
 
-[<Test>]
-let testExecute() = 
-    let ex1 = 
+    [<Test>]
+    let testExecute() = 
         ExecutionModule.checkGoal (Goal("eq1", [va "N"])) [Rule(Signature("eq1", [vp "N"]), (EqExpr(sv "N", sn 1.)))]
         |> check "execute1" [[sn 1.]]
-    let ex2 = 
+
         ExecutionModule.checkGoal (Goal("eq2", [sna 1.])) [Rule(Signature("eq2", [vp "N"]), (EqExpr(sv "N", sn 1.)))]
         |> check "execute2" [[sn 1.]]
-    let ex3 = 
+
         ExecutionModule.checkGoal (Goal("eq3", [sna 2.])) [Rule(Signature("eq3", [vp "N"]), (EqExpr(sv "N", sn 1.)))]
         |> check "execute3" []
             
-    let andCheck =
         ExecutionModule.checkGoal (Goal("and", [va "N"])) [Rule(Signature("and", [vp "N"]), (AndExpression(EqExpr(sv "N", sn 1.), EqExpr(sv "N", sn 2.))))]
         |> check "execute3" []
 
-    let orCheck =
         ExecutionModule.checkGoal (Goal("or", [va "N"])) [Rule(Signature("or", [vp "N"]), (OrExpression(EqExpr(sv "N", sn 1.), EqExpr(sv "N", sn 2.))))]
         |> check "execute3" [[sn 1.]; [sn 2.]]
 
-    let falseCheck =
         ExecutionModule.checkGoal (Goal("fa", [va "N"])) [Rule(Signature("fa", [vp "N"]), (False))]
         |> check "false check" []
 
-    let innerVariableCheck =
         ExecutionModule.checkGoal (Goal("innervar", [va "N"])) [Rule(Signature("innervar", [vp "N"]), (AndExpression(EqExpr(sv "Temp", sn 1.), EqExpr(sv "N", sv "Temp"))))]
         |> check "innervar not supported" [[sn 1.]]
-    ()
 
-[<Test>]
-let realTest() =
-    let eq1 = 
+    [<Test>]
+    let realTest() =
         ExecutionModule.checkGoal (Goal("eq1_both", [va "N"; va "Res"])) [Rule(Signature("eq1_both", [vp "N1"; vp "N2"]), (AndExpression((EqExpr(sv "N1", sn 1.), (EqExpr(sv "N2", sn 1.))))))]
         |> check "and test1" [[sn 1.; sn 1.]]
-    let eq2 = 
         ExecutionModule.checkGoal(Goal("eq", [va "N"; va "N2"])) [Rule(Signature("eq", [vp "N1"; vp "N2"]), (EqExpr(sv "N1", sv "N2")))]
         |> check "eq test" [[sv "N2"; sv "N2"]]
 
-    let f = Rule(Signature("f1", [vp "N"; vp "Res"]), OrExpression(AndExpression(EqExpr(sv "N", sn 1.), EqExpr(sv "Res", sn 1.)), AndExpression(GrExpr(sv "N", sn 1.), EqExpr(sv "Res", sn 2.))))
-    let f1 = 
-        ExecutionModule.checkGoal (Goal("f1", [sna 1.; va "Res"])) [f]
+        let pseudoFactorialRule = Rule(Signature("f1", [vp "N"; vp "Res"]), OrExpression(AndExpression(EqExpr(sv "N", sn 1.), EqExpr(sv "Res", sn 1.)), AndExpression(GrExpr(sv "N", sn 1.), EqExpr(sv "Res", sn 2.))))
+        ExecutionModule.checkGoal (Goal("f1", [sna 1.; va "Res"])) [pseudoFactorialRule]
         |> check "f1" [[sn 1.; sn 1.]]
-    let f2 =
-        ExecutionModule.checkGoal (Goal("f1", [sna 3.; va "Res"])) [f]
+        ExecutionModule.checkGoal (Goal("f1", [sna 3.; va "Res"])) [pseudoFactorialRule]
         |> check "f2" [[sn 3.; sn 2.]]
-    ()
+
+[<TestFixture>]
+module RuleTests =
+    let person p = Rule(Signature("person", [Parameter(stringAny p)]), True)
+    let parent p d = Rule(Signature("parent", [Parameter(stringAny p); Parameter(stringAny d)]), True)
+    let grandparent = Rule(Signature("grandparent", [vp "G"; vp "D"]), AndExpression(CallExpression(Goal("parent", [va "G"; va "P"])), CallExpression(Goal("parent", [va "P"; va "D"]))))
+
+    let knowledgebase = [
+        person "Mary";
+        person "Polina";
+        person "Evgeniy";
+        person "Solniwko";
+    
+        parent "Mary" "Polina";
+        parent "Solniwko" "Polina";
+        parent "Polina" "Evgeniy";
+
+        grandparent
+    ]
+
+    [<Test>]
+    let testPersonRule() =
+        ExecutionModule.checkGoal (Goal("person", [Argument(stringAny "Polina")])) knowledgebase
+        |> check "check polina" [[stringAny "Polina"]]
+        ExecutionModule.checkGoal (Goal("person", [va "X"])) knowledgebase
+        |> check "check people" [[stringAny "Mary"]; [stringAny "Polina"]; [stringAny "Evgeniy"]; [stringAny "Solniwko"]]
+        ExecutionModule.checkGoal (Goal("person", [Argument(stringAny "Miwa")])) knowledgebase
+        |> check "check missing person" []
+
+    [<Test>]
+    let testParentRule() =
+        ExecutionModule.checkGoal (Goal("parent", [Argument(stringAny "Polina"); va "Descendant"])) knowledgebase
+        |> check "check defined parent" [[stringAny "Polina"; stringAny "Evgeniy"]]
+        ExecutionModule.checkGoal (Goal("parent", [va "Parent"; va "Descendant"])) knowledgebase
+        |> check "check all parents" [[stringAny "Mary"; stringAny "Polina"]; [stringAny "Solniwko"; stringAny "Polina"]; [stringAny "Polina"; stringAny "Evgeniy"]]
+
+    [<Test>]
+    let testGrandparentRule() =
+        ExecutionModule.checkGoal (Goal("grandparent", [va "GrandParent"; va "Descendant"])) knowledgebase
+        |> check "check all parents" [[stringAny "Mary"; stringAny "Evgeniy"]; [stringAny "Solniwko"; stringAny "Evgeniy"]]
+        ExecutionModule.checkGoal (Goal("grandparent", [Argument(stringAny "Mary"); Argument(stringAny "Evgeniy")])) knowledgebase
+        |> check "check defined parent" [[stringAny "Mary"; stringAny "Evgeniy"]]
