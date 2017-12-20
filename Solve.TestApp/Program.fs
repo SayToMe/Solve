0336efbc8a5c331bb7ac39cf7ab8ab6f33894e6d
Qrtic <smirnov_pasha@mail.ru>
2017-12-21 00:11:12 +0300

    Improved interactive

--- Solve.TestApp/Program.fs
+++ Solve.TestApp/Program.fs
@@ -9,16 +9,32 @@ open TermTypes.Transformers
 open Rule
 open Rule.Transformers
 
+open System
+
 [<EntryPoint>]
 let main argv = 
     let i = Solve.Interactive()
 
-    let mutable input = System.Console.ReadLine()
+    let mutable input = Console.ReadLine()
     while input <> "exit" do
         try
             match i.NewInput input with
             | RuleInfo(r) -> printfn "%A" r
-            | SolveResult(r) -> printfn "%A" r
+            | SolveResult(r) ->
+                let rec proc seq checkKey =
+                    let checkBackTrack() =
+                        let rk = Console.ReadKey()
+                        let key = rk.Key
+                        key = ConsoleKey.Enter || rk.KeyChar = ';'
+
+                    if not checkKey || checkBackTrack() then
+                        match Seq.tryHead seq with
+                        | Some answ -> 
+                            printfn "%A" (answ)
+                            proc (Seq.tail seq) true
+                        | None -> printfn "fail"
+                proc r false
+                printfn ""
             | _ -> ()
         with
         | _ as e -> printfn "Failed to parse previous input"
