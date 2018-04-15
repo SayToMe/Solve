a5067ae88b9df1f05999baf73d9c50c52b298e2f
saytome <smirnov_pasha@mail.ru>
2018-04-15 03:32:54 +0300

    First implementation of terminal abstraction

--- examples/Solve.TestApp/Program.fs
+++ examples/Solve.TestApp/Program.fs
@@ -1,8 +1,10 @@
 ﻿open Solve
-open Solve.Parse
+open Solve.PrologParser
 
 open System
 open System.Text.RegularExpressions
+
+open Solve.Terminal
 
 // ParseRegex parses a regular expression and returns a list of the strings that match each group in
 // the regular expression.
@@ -14,121 +16,55 @@ let (|ParseRegex|_|) regex str =
    then Some (List.tail [ for x in m.Groups -> x.Value ])
    else None
 
-type Mode =
-    | Insert
-    | Query
-    | Exit
-
-type BackTrackMode =
-    | NoBackTrack
-    | SingleBackTrack
-    | AlwaysBackTrack
-
 let deletePrevConsoleLine() =
     if Console.CursorTop > 0 then
         Console.Write(new string(' ', Console.WindowWidth))
         Console.SetCursorPosition(0, Console.CursorTop - 1)
-        
-let prepareInput mode =
-    match mode with
-    | Insert -> printf ":-"
-    | Query -> printf "?-"
-    | Exit -> ()
 
-    let input = Console.ReadLine()
-    match mode with
-    | Insert -> ":-" + input
-    | Query -> "?-" + input
-    | Exit -> ""
+type ConsoleTerminal() =
+    let kb: IKnowledgebaseWrapper = MutableKnowledgebase([]) :> IKnowledgebaseWrapper
+
+    interface ITerminal with
+        member __.Solve goal = kb.Solve goal
+        member __.Insert rule = kb.AddRule rule
+        member __.Log logMessage =
+            match logMessage with
+            | ModeLog mode -> printf "%s" mode.AsString
+            | InfoLog info -> printfn "%s" info
+            | SuccessInsertionLog(name, arity) -> printfn "Success insertion of %s/%i" name arity
+            | ResultLog res -> printfn "%s" res
+            | ErrorLog error -> printfn "Error: %s" error
+        member __.ReadInput() = Console.ReadLine()
+        member __.ReadKey() = Console.ReadKey()
+
+// Factorial example
+// factorial(1,1).
+// factorial(X,Y) :- X > 1, X1 is X - 1, factorial(X1, Y1), Y is X * Y1.
+
+// Parent example
+// >>> person(name)
+// person(masha).
+// person(misha).
+// person(alex).
+// person(sova).
+// >>> parent(Parent, Child)
+// parent(alex, masha).
+// parent(alex, sova).
+// parent(masha, misha).
+// >>> grandparent(GrandParent, GrandChild)
+// grandparent(GP, GC) :- parent(GP, P), parent(P, GC).
 
 [<EntryPoint>]
-let main argv = 
-    let interactive = Solve.Parse.Interactive()
-    
-    printfn "Choose mode by typing: 'insert' (:-) or 'query' (?-)."
+let main argv =
+    let terminal = ConsoleTerminal()
 
-    // Factorial example
-    // factorial(1,1).
-    // factorial(X,Y) :- X > 1, X1 is X - 1, factorial(X1, Y1), Y is X * Y1.
+    Welcome
+    |> Seq.unfold (
+        function
+        | End -> None
+        | _ as mode -> Some <| (0, run terminal mode)
+    )
+    |> Seq.iter ignore
 
-    // Parent example
-    // >>> person(name)
-    // person(masha).
-    // person(misha).
-    // person(alex).
-    // person(sova).
-    // >>> parent(Parent, Child)
-    // parent(alex, masha).
-    // parent(alex, sova).
-    // parent(masha, misha).
-    // >>> grandparent(GrandParent, GrandChild)
-    // grandparent(GP, GC) :- parent(GP, P), parent(P, GC).
-
-    let rec cycle mode =
-        match mode with
-        | Exit -> ()
-        | Insert
-        | Query ->
-            try
-                let input = prepareInput mode
-                match input with
-                | ParseRegex "^[:?]-\s*exit\s*$" _ ->
-                    cycle Exit
-                | ParseRegex "^[:?]-\s*insert\s*$" _ ->
-                    cycle Insert
-                | ParseRegex "^[:?]-\s*query\s*$" _ ->
-                    cycle Query
-                | _ ->
-                    match interactive.NewInput input with
-                    | RuleInfo(Rule.Rule(sign, _)) as r -> 
-                        printfn "Insertion succeed: %O." sign
-                        printfn "Rule: %A" r
-
-                        cycle mode
-                    | SolveResult(results) ->
-                        let getBackTrackMode() =
-                            let rk = Console.ReadKey()
-                            deletePrevConsoleLine()
-                            match rk.Key with
-                            | ConsoleKey.Escape -> NoBackTrack
-                            | ConsoleKey.Spacebar
-                            | ConsoleKey.Enter -> SingleBackTrack
-                            | ConsoleKey.A -> AlwaysBackTrack
-                            | _ ->
-                                match rk.KeyChar with
-                                | ';' -> SingleBackTrack
-                                | '.' -> NoBackTrack
-                                | _ -> SingleBackTrack
-                        results
-                        |> Seq.unfold (fun st ->
-                            match Seq.tryHead st with
-                            | Some answ ->
-                                let showAnswer answ = if answ = "" then printfn "true" else printfn "%A" answ
-
-                                showAnswer answ
-
-                                match getBackTrackMode() with
-                                | SingleBackTrack -> Some (answ, Seq.tail st)
-                                | AlwaysBackTrack -> 
-                                    Seq.iter showAnswer (Seq.tail st)
-                                    printfn "false"
-                                    None
-                                | NoBackTrack -> None
-                            | _ ->
-                                printfn "false"
-                                None
-                        )
-                        |> Seq.iter ignore
-
-                        cycle mode
-                    | Error e ->
-                        printfn "Error. %A" e
-                        cycle mode
-                    | NoResult -> cycle mode
-            with
-            | _ as e ->
-                printfn "Failure. %A" e.Message
-                cycle mode
-
-    cycle Insert
+    Console.ReadKey() |> ignore
     0
---
+++ src/Solve.Terminal/Terminal.fs
@@ -0,0 +1,115 @@
+﻿namespace Solve.Terminal
+
+open Solve
+open Solve.Rule
+open Solve.PrologParser
+open Solve.PrologParser.Parser
+
+open System
+
+[<StructuredFormatDisplay("{AsString}")>]
+type TerminalMode =
+    | Welcome
+    | Insert
+    | Query
+    | Exit
+    | End
+    with
+    member self.AsString =
+        match self with
+        | Welcome -> "Welcome to terminal. Choose mode by typing: 'insert' (:-) or 'query' (?-)."
+        | Insert -> ":-"
+        | Query -> "?-"
+        | Exit -> "Exit..."
+        | End -> failwith "Should not be called"
+
+type BackTrackMode =
+    | NoBackTrack
+    | SingleBackTrack
+    | AlwaysBackTrack
+    with
+        static member Parse (keyInfo: ConsoleKeyInfo) =
+            match keyInfo.Key with
+            | ConsoleKey.Escape -> NoBackTrack
+            | ConsoleKey.Spacebar
+            | ConsoleKey.Enter -> SingleBackTrack
+            | ConsoleKey.A -> AlwaysBackTrack
+            | _ ->
+                match keyInfo.KeyChar with
+                | ';' -> SingleBackTrack
+                | '.' -> NoBackTrack
+                | _ -> SingleBackTrack
+
+type LogMessage =
+    | ModeLog of TerminalMode
+    | SuccessInsertionLog of string * int
+    | ResultLog of string
+    | ErrorLog of string
+    | InfoLog of string
+
+type ITerminal =
+    abstract member Solve: Goal -> string seq
+    abstract member Insert: Rule -> unit
+    abstract member Log: LogMessage -> unit
+    abstract member ReadInput: unit -> string
+    abstract member ReadKey: unit -> ConsoleKeyInfo
+
+[<AutoOpen>]
+module TerminalRunners =
+    let consumeInput (terminal: ITerminal) (mode: TerminalMode) (input: string) =
+        let fullPlString = mode.AsString + input
+        match parsePlString fullPlString with
+        | RuleParseResult rule -> 
+            terminal.Insert rule
+        | CallParseResult goal ->
+            terminal.Solve (Goal(goal))
+            |> Seq.unfold (fun st ->
+                match Seq.tryHead st with
+                | Some answ ->
+                    let showAnswer answ = if answ = "" then terminal.Log (ResultLog "true") else terminal.Log (ResultLog (sprintf "%A" answ))
+
+                    showAnswer answ
+
+                    let backtrackMode = BackTrackMode.Parse(terminal.ReadKey())
+                    match backtrackMode with
+                    | SingleBackTrack -> Some (answ, Seq.tail st)
+                    | AlwaysBackTrack -> 
+                        Seq.iter showAnswer (Seq.tail st)
+                        printfn "false"
+                        None
+                    | NoBackTrack -> None
+                | _ ->
+                    terminal.Log (ResultLog "false")
+                    None
+            )
+            |> Seq.iter ignore
+        | ParseError error ->
+            terminal.Log (ErrorLog error)
+
+    let run (terminal: ITerminal) (mode: TerminalMode) =
+        match mode with
+        | Welcome ->
+            terminal.Log (InfoLog mode.AsString)
+            Insert
+        | Insert
+        | Query ->
+            try
+                terminal.Log (ModeLog mode)
+
+                let input = terminal.ReadInput()
+                match input with
+                | "exit"
+                | "e"
+                | "q" ->
+                    Exit
+                | _ ->
+                    consumeInput terminal mode input
+                    mode
+            with
+            | _ as e ->
+                printfn "Failure. %A" e.Message
+                mode
+        | Exit ->
+            terminal.Log (InfoLog mode.AsString)
+            End
+        | End -> failwith "Should not be called"
