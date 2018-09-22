741a96c1e32f8237da6ec3fa3bea9353fba213be
Smirnov Pavel <11567655+SayToMe@users.noreply.github.com>
2018-09-22 13:25:06 +0300

    Feature/new tests (#13)
    * Add new tests based on input
    
    * Add more tests and fixed FSharp.Core

--- src/Solve.Terminal/Knowledgebase.fs
+++ src/Solve.Terminal/Knowledgebase.fs
@@ -9,6 +9,21 @@ open Solve.PrologParser
 open Solve.PrologParser.Primitives
 open Solve.PrologParser.Parser
 
+[<AutoOpen>]
+module SolveResultExtensions =
+    let convertSolveResult (results: ((Variable * Term) list) seq) =
+        results
+        |> Seq.map (fun result ->
+           result
+           |> List.fold (fun acc (v, t) ->
+               let str = (VariableTerm(v).AsString + " = " + t.AsString)
+               if String.length acc = 0 then
+                   str
+               else
+                   acc + ", " + str
+           ) ""
+        )
+
 type IKnowledgebaseWrapper =
     abstract member AddRule: Rule -> unit
     abstract member Solve: Goal -> string seq
@@ -17,20 +32,11 @@ type MutableKnowledgebase(initialKnowledgebase: Rule list) =
     let mutable innerKnowledgebase = initialKnowledgebase
 
     member __.Rules = innerKnowledgebase
-
+        
     interface IKnowledgebaseWrapper with
         member __.AddRule rule =
             innerKnowledgebase <- innerKnowledgebase @ [rule]
 
         member __.Solve (Goal(goal)) =
             Solve.solve goal innerKnowledgebase
-            |> Seq.map (fun result ->
-                result
-                |> List.fold (fun acc (v, t) ->
-                    let str = (VariableTerm(v).AsString + " = " + t.AsString)
-                    if String.length acc = 0 then
-                        str
-                    else
-                        acc + ", " + str
-                ) ""
-            )
+            |> convertSolveResult
